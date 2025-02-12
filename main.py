f79e59ba60e14bab724f2c1af0f995022ee3adcb
meganasampson <megan.anne.sampson@gmail.com>
2025-02-12 15:27:50 +0000

    setup

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("main file")
+import pandas as pd
+
+
+
+#a1 = [1,2,3,4]
+#b1 = [1,2,3,4]
+#df = pd.DataFrame({'a2': a1, 'b2': b1})
+#df.to_excel('test.xlsx', sheet_name='sheet1', index=False)
