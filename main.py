436ad0378569d3deba4062d1b810ef866e573aa9
meganasampson <megan.anne.sampson@gmail.com>
2025-03-11 10:54:09 +0000

    renaming of columns to be one line

--- main.py
+++ main.py
@@ -5,23 +5,49 @@ import datetime
 #row 14 is starting row
 #1-17
 
+def datetimefix(column, min = 12, max = 23):
+    for i in range(min,max):
+        if type(column[i]) == datetime.time:
+            date = datetime.datetime(2099, 12, 30).date()
+            column[i] = date
+        elif type(column[i]) == datetime.datetime:
+            date = column[i].date()
+            column[i] = date
+
+
 df1 = pd.read_excel('data/SQ2024-1450 CAT SC_LCF Summary Data (100924).xlsx', sheet_name='Report Tables', keep_default_na=False)
 df2 = pd.read_excel('data/SQ2024-1450 CAT SC_LCF Summary Data (100924).xlsx', sheet_name='Report Tables', keep_default_na=False)
-df3 = pd.read_excel('data/SQ2024-1450 CAT SC_LCF Summary Data (100924).xlsx',
-                     sheet_name='Report Tables', keep_default_na=False,usecols="C" ,skiprows=[0,1,2,3,4,5,6,7,8])
 
 
 collist = list(df1)
 nums = [x for x in range(len(collist))]
 df1.columns = nums
 
-example = df2.loc[11:23, 'Unnamed: 1':]
-example = example.reset_index(drop = True)
-collist2 = list(example)
-nums2 = [x for x in range(len(collist2))]
-example.columns = nums2
+namelist = []
 
+columnnames = df2.loc[9:11]
+for i in columnnames:
+    aname = columnnames.loc[9,i]+columnnames.loc[10,i]+columnnames.loc[11,i]
+    namelist.append(aname) # list of column names
 
+namelist = namelist[1:] # to get rid of the first empty element
+example = df2.loc[12:23, 'Unnamed: 1':] # takes values WITHOUT name of columns
+example = example.reset_index(drop = True) # starts new row index at 1
+example.columns = namelist # renames the columns to the original name (but one line)
+
+# old example of renaming the columns to be numbers
+#collist2 = list(example)
+#nums2 = [x for x in range(len(collist2))]
+#example.columns = nums2
+
+#testcol17 = df2.loc[12:23,17] # start date
+#testcol18 = df2.loc[12:23,18] # end date
+
+#datetimefix(testcol17)
+#datetimefix(testcol18)
+
+with pd.ExcelWriter("test2.xlsx") as writer:
+    example.to_excel(writer)
 
 """
 col1 = df1.loc[:,"Unnamed: 1"]
@@ -44,20 +70,10 @@ col17 = df1.loc[:,"Unnamed: 17"]"""
 
 
 #colrange = df1[df1.columns[15:18]]
-#print(colrange)
 
 col17 = df1.loc[12:23,17] # start date
 col18 = df1.loc[12:23,18] # end date
 
-
-def datetimefix(column, min = 12, max = 23):
-    for i in range(min,max):
-        if type(column[i]) == datetime.time:
-            date = datetime.datetime(2099, 12, 30).date()
-            column[i] = date
-        elif type(column[i]) == datetime.datetime:
-            date = column[i].date()
-            column[i] = date
 
 datetimefix(col17)
 datetimefix(col18)
@@ -67,6 +83,4 @@ with pd.ExcelWriter("test.xlsx", date_format="YYYY-MM-DD") as writer:
     df1.to_excel(writer)  
 
 
-with pd.ExcelWriter("test2.xlsx") as writer:
-    example.to_excel(writer)
 
