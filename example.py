0281a13725b85a2ea8c3cfdce84bd4176710f4a0
meganasampson <megan.anne.sampson@gmail.com>
2025-09-24 13:45:30 +0100

    added tests folder

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import dbimporter as dbi
 
-dbi.structcheck.Check(filename="src/dbimporter/data/baddata.xlsx")
+#dbi.structcheck.Check(filename="src/dbimporter/data/baddata.xlsx")
 #dbi.structcheck.Check(filename="src/dbimporter/data/realdataex.xlsx")
+dbi.restructure.test()
