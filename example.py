5dcf34c5be3f8abaf3566ed7cf7a29bb37581756
meganasampson <megan.anne.sampson@gmail.com>
2026-06-16 14:27:40 +0100

    add template for visual information in xlsx

--- example.py
+++ example.py
@@ -3,4 +3,11 @@ import dbimporter as dbi
 
 #dbi.check_structure.Check(filename="src/dbimporter/data/find_unit_test.xlsx", automatic_start=True)
 
-dbi.check_structure.Check(filename="src/dbimporter/data/Rod #8- 3.4.2-10%-test data at cryo -75C.xlsx", file_type = "cryotensile")
+#dbi.check_structure.Check(filename="src/dbimporter/data/Rod #8- 3.4.2-10%-test data at cryo -75C.xlsx", file_type = "cryotensile")
+
+# sheetnames, sheets = dbi.graph_extract.read_data(filename="src/dbimporter/data/Rod #8- 3.4.2-10%-test data at cryo -75C.xlsx")
+# sheetdata = sheets[sheetnames[0]]
+
+# print(sheetdata.head(3))
+
+dbi.graph_extract.get_image_test(filename="src/dbimporter/data/Rod #8- 3.4.2-10%-test data at cryo -75C.xlsx")
